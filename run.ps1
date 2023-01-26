30aaee59c76df102a26cf96019261828859ed33e
masachika <masachika.kmd@gmail.com>
2023-01-26 22:37:48 +0900

    pcd保存のためのカスタマイズ追加

--- run.ps1
+++ run.ps1
@@ -1,5 +1,7 @@
 docker run --init -it -d `
     -v ${PWD}/dataset:/root/catkin_ws/src/LIO-SAM/dataset `
+    -v ${PWD}/config/params.yaml:/root/catkin_ws/src/LIO-SAM/config/params.yaml `
+    -v ${PWD}/pcd:/root/catkin_ws/src/LIO-SAM/pcd `
     --name=liosam-melodic `
     liosam-melodic `
     bash
